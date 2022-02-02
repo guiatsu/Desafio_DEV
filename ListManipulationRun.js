82dc8507385fd2e9b662760489ec8f1a951287bc
Guiatsu <yuricrystian@hotmail.com>
2022-02-02 13:31:48 -0300

    feito algumas correções para caso sejam utilizadas entradas com multiplos espaços ou virgulas

--- ListManipulationRun.js
+++ ListManipulationRun.js
@@ -6,6 +6,12 @@ const rl = readline.createInterface({
 });
 
 rl.question('Enter a list of numbers separated by comma:\n', function (numbers) {
+	numbers = numbers.replace(/[\s]+/g,',').replace(/[,]+/g,',');
+	if(numbers.search("[^1-9,]") != -1){
+		console.log("lista invalida, por favor insira apenas numeros inteiros")
+		rl.close();
+		process.exit(0);
+	}
     var list = numbers.split(",").map(num => parseInt(num,10));
     console.log(`input:[${list}]`);
 	list = ListManipulation(list);
